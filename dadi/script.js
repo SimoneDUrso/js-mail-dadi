c26fd9a2286795eab5b03872be1bcefb13fb19bf
SimoneDUrso <simone.durso98@gmail.com>
2024-06-28 17:07:35 +0200

    [UPD] Created variable "lancia" on JS file

--- dadi/script.js
+++ dadi/script.js
@@ -1,6 +1,6 @@
-let lanciaDadi = document.getElementById("lanciaDadi");
-lanciaDadi.addEventListener("click", function () {
+let lancia = document.getElementById("lancia");
+lancia.addEventListener("click", function () {
 
-    let dadoGiocatore = Math.floor(math.random() * 6) + 1;
-    let dadoComputer = Math.floor(math.random() * 6) + 1;
+
+
 })
