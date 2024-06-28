06a0d926eb9bdf5d16046ec0effd710d1794e087
SimoneDUrso <simone.durso98@gmail.com>
2024-06-28 17:18:01 +0200

    [UPD] ESERCIZIO DADI - Created if

--- dadi/script.js
+++ dadi/script.js
@@ -4,6 +4,12 @@ lancia.addEventListener("click", function () {
     const dadoGiocatore = Math.floor(Math.random() * 6) + 1;
     const dadoComputer = Math.floor(Math.random() * 6) + 1;
 
+    let messaggio = `Hai lanciato un ${dadoGiocatore} e il computer ha lanciato un ${dadoComputer}. `;
+
+    if (dadoGiocatore > dadoComputer) {
+        messaggio += "Hai vinto!";
+    }
+
 
 
 })
