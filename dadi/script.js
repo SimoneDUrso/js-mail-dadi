aaca718208ac001094bb029ce9c76e775b989924
SimoneDUrso <simone.durso98@gmail.com>
2024-06-28 17:17:31 +0200

    [UPD] ESERCIZIO DADI - Created variables "dadoGiocatore" and "dadoComputer"

--- dadi/script.js
+++ dadi/script.js
@@ -1,6 +1,9 @@
 let lancia = document.getElementById("lancia");
 lancia.addEventListener("click", function () {
 
+    const dadoGiocatore = Math.floor(Math.random() * 6) + 1;
+    const dadoComputer = Math.floor(Math.random() * 6) + 1;
+
 
 
 })
