8313058bb4780f4b9a4ef98a200da1299b32cf9f
SimoneDUrso <simone.durso98@gmail.com>
2024-06-28 16:10:35 +0200

    [UPD] Added variable "emailAutorizzate" and button event

--- mail/script.js
+++ mail/script.js
@@ -1,2 +1,11 @@
+const emailAutorizzate = ["info@paoloverdi.it", "studiolegale@paoloverdi.it", "paolo@studiolegalepaoloverdi.it", "esempio4@gmail.com", "esempio5@gmail.com"];
 
 
+
+// Controlla se l'email è nella lista
+let btn_verifica = document.getElementById("btn-verifica");
+btn_verifica.addEventListener("click", function () {
+    let emailInput = document.getElementById("email").value;
+    let autorizzato = false;
+
+})
